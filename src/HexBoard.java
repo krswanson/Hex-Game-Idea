4814742064d4f04a63f99c5835a32b4c385cad0d
krswanson <Kristin@swanson.name>
2013-05-07 13:19:23 -0700

    Updated HexBoard to improve tile indexing

--- src/HexBoard.java
+++ src/HexBoard.java
@@ -1,39 +1,120 @@
 import java.awt.Polygon;
-import java.util.Vector;
+import java.util.Iterator;
 
 public class HexBoard {
 
 	protected Polygon p;
-	protected Vector<Polygon> hexes;
+	//The board is setup [column][row] but just use the get method
 	protected Polygon[][] board;
 	protected int h, w, columns;
 	
-	public HexBoard(int hexW, int hexH, int[] perCol, int winW, int winH){
+	public HexBoard(int[] perCol, int winW, int winH){
+		this(perCol, winW, winH, 40, 32);
+	}
+	
+	public HexBoard(int[] perCol, int winW, int winH, int hexW, int hexH){
 		columns = perCol.length;
-		hexes = new Vector<Polygon>(columns*4);
-		//board = new Polygon[columns][];
+		//hexes = new Vector<Polygon>(columns*4);
+		board = new Polygon[columns][];
 		h = hexH;
 		w = hexW;
 		int[] xpoints = {w/4,w-w/4,w,w-w/4,w/4,0};
 		int[] ypoints = {0,0,h/2,h,h,h/2};
 		p = new Polygon(xpoints, ypoints, 6);
-		p.translate(winW/2-w*columns/2, winH/2-h*perCol[(int)(columns/2)]/2);
+		p.translate((int)(winW/2-w*columns/2+.5*w)/1, 
+				(int)(winH/2-h*perCol[(int)(columns/2)]/2+.5*h));
 		
-		//find largest
-		int i, correction;
+		//Each row array has its own length
+		int i, correction = 0;
 		for (int j = 0; j < columns; j++){
+			Polygon[] thisCol = new Polygon[perCol[j]];
 			for (i = 0; i < perCol[j]; i++){
 				p.translate(0, h);
-				hexes.add(new Polygon(p.xpoints, p.ypoints, p.npoints));
-				
+				thisCol[i] = new Polygon(p.xpoints, p.ypoints, p.npoints);
 			}
+			board[j] = thisCol;
+			
 			if (j == columns-1) break;
-			else if (perCol[j] <= perCol[j+1]) correction = -h/2;
-			else correction = h/2;
+			else if (perCol[j] < perCol[j+1]) correction = -h/2;
+			else {// column is == or >, if it is ==, then do the opposite of what was just done
+				if (perCol[j] == perCol[j+1] && correction == h/2) correction = -h/2;
+				else correction = h/2;
+			}
 			p.translate(w*3/4, -i*h + correction);
 		}
 	}
+	
+	public Polygon get(int row, int col){
+		return board[col][row];
+	}
+	
+	public boolean removeHex(int row, int col){
+		if (row >= board[col].length) return false;
+		Polygon[] temp = board[col];
+		board[col] = new Polygon[board[col].length-1];
+		int t = 0, b = 0;
+		while (b < board[col].length){
+			if (t == row){
+				t++;
+				continue;
+			}
+			board[col][b] = temp[t];
+			t++;
+			b++;
+		}
+		return board[col].length < temp.length;
+	}
+	
+	public void compress(){
+		System.out.println("The method compress current does nothing.");
+	}
+	
+	public Iterator<Polygon> iterator(){
+		return new BoardIterator();
+	}
+	
+	protected class BoardIterator implements Iterator<Polygon>{
+		int row, col;
+		
+		public BoardIterator(){
+			reset();
+		}
+		
+		public void reset(){
+			row = 0;
+			col = 0;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			if (row == board[columns-1].length && col == columns-1)
+				return false;
+			else return true;
+		}
 
+		@Override
+		public Polygon next() {
+			ensureRow();
+			Polygon current = board[col][row];	
+			row++;
+			return current;
+		}
+
+		@Override
+		public void remove() {
+			ensureRow();
+			removeHex(row, col);
+		}
+		
+		private void ensureRow(){
+			if (row == board[col].length){
+				row = 0;
+				col++;
+			}
+		}
+	}
+		
+	
 	/**
 	 * @param args
 	 */
@@ -42,4 +123,5 @@ public class HexBoard {
 
 	}
 
+	
 }
--- src/TheGame.java
+++ src/TheGame.java
@@ -2,12 +2,15 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Polygon;
+import java.util.Iterator;
 
 
+
+@SuppressWarnings("serial")
 public class TheGame extends Frame {
 
-	protected int W = 500, H = 400, w = 40, h = 32;
-	protected HexBoard b = new HexBoard(w,h, new int[] {3,4,5,4,3}, W, H);
+	protected int W = 500, H = 250;
+	protected HexBoard b = new HexBoard(new int[] {2,3,3,3,4,5,4,3,3,3,2}, W, H);
 	
 	public TheGame(){
 		
@@ -17,21 +20,29 @@ public class TheGame extends Frame {
 	}
 	
 	public void paint(Graphics g){
-		
-		for (int i = 0; i < b.hexes.size(); i++){
+		Iterator<Polygon> hexes = b.iterator();
+		int i = 0;
+		while (hexes.hasNext()){
 			if (i%3==0) g.setColor(Color.GREEN);
 			else if (i%3 ==1) g.setColor(Color.YELLOW);
 			else g.setColor(Color.RED);
-			
-			g.fillPolygon(b.hexes.get(i));
+			Polygon p = hexes.next();
+			g.fillPolygon(p);
 			g.setColor(Color.BLACK);
-			g.drawPolygon(b.hexes.get(i));
-			
+			g.drawPolygon(p);
+			i++;
 		}
+		
 	}
 	
 	public static void main(String[] args) {
 		TheGame g = new TheGame();
+		System.out.println(g.b.removeHex(1,1));
+		System.out.println(g.b.removeHex(1,5));
+		System.out.println(g.b.removeHex(1,5));
+		System.out.println(g.b.removeHex(3,1));
+		System.out.println("In Main " + g.b.board[1].length + " " + g.b.board[5].length);
+		
 	}
 
 }
